0ba4e5355347efa799d3e7924042bbe5a11ffc1e
KIMJONGJINN <enjoy1358@gmail.com>
2023-02-21 22:47:33 +0900

    카카오톡 틀 만들기

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { Profiler } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 /**
  * Header
@@ -10,9 +10,80 @@ import { StyleSheet, View } from "react-native";
  * FriendList
  */
 
-export default function App() {
-  return <View style={styles.container}></View>;
+ const Header = (props) => {
+   return <Text>{props.title}</Text>
+ }
+ const MyProfile = () => {
+  return <Profile name="종진" uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg" 
+  profilesize ={40}/>
 }
+const Division = () => {
+  return <Text>Division</Text>
+}
+const FriendSection = () => {
+  return <Text>FriendSection</Text>
+}
+const FriendList = () => {
+  return (
+    <View>
+      <Profile 
+      name = "친구1"
+      uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg"
+      profilesize={30}
+      />
+      <Profile 
+      name = "친구2"
+      uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg"
+      profilesize={30}
+      />
+      <Profile 
+      name = "친구3"
+      uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg"
+      profilesize={30}
+      />
+      <Profile 
+      name = "친구4"
+      uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg"
+      profilesize={30}
+      />
+      <Profile 
+      name = "친구5"
+      uri="https://blog.kakaocdn.net/dn/uqGMx/btq4l9SMqEV/jofd0jUhc2LIviOmIcMAzk/img.jpg"
+      profilesize={30}
+      />
+    </View>
+  );
+}
+
+const Profile = (props) => {
+  return (
+    <View style={{flexDirection : "row"}}>
+      <Image
+        source ={{
+          uri: props.uri
+        }}
+        style={{
+          width: props.profilesize,
+          height : props.profilesize,
+        }}
+        />
+
+        <Text>{props.name}</Text>
+    </View>
+  )
+}
+
+
+export default function App() {
+  return <View style={styles.container}>
+    <Header title="친구"/>
+    <MyProfile />
+    <Division />
+    <FriendSection />
+    <FriendList/>
+  </View>;
+}
+
 
 const styles = StyleSheet.create({
   container: {
